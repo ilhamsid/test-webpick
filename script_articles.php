5ee16dbd3ce94b3df1dc60a591bc388a2cb08f06
ilhamsid <siddik.ilham@gmail.com>
2021-04-23 15:50:48 +0000

    Création articles

--- script_articles.php
+++ script_articles.php
@@ -3,53 +3,7 @@ require(dirname(__FILE__) . '/wp-load.php');
 require_once( ABSPATH . '/wp-admin/includes/taxonomy.php');
 require_once( ABSPATH . '/wp-admin/includes/post.php');
 
-// lecture fichier csv
-function read($csv){
-    $file = fopen($csv, 'r');
-    while (!feof($file) ) {
-        $line[] = fgetcsv($file, 1024);
-    }
-    fclose($file);
-    return $line;
-}
-// fichier des marques/modèles
-$csv = 'csv_marque_Modele_290318.csv';
-
-
-/*** création catégories ***/
-$cat_name = "Marques auto";
-$cat_id = create_category("Catégorie", $cat_name);
-
-$csv = read($csv);
-for ($i=1; $i < count($csv)-1; $i++) { 
-	$line = $csv[$i][0];
-	$explode = explode(';', $line);
-
-	$marque_name = iconv('UTF-8', 'ISO-8859-1//IGNORE', $explode[0]);
-	$modele_name = iconv('UTF-8', 'ISO-8859-1//IGNORE', $explode[1]);
-
-	$marque_id = create_category("Marque", $marque_name, $cat_id);
-	$modele_id = create_category("Modèle", $modele_name, $marque_id);
-}
-
-function create_category($type, $name, $parent=null){
-	$id = category_exists($name);
-	if($id)
-		echo 'La catégorie "'.$type.' : '.$name.'" existe déjà !<br>';
-	else{
-		wp_insert_category(
-	        array(
-	            'cat_name'        => $name,
-	            'category_parent' => $parent,
-	        )
-	    );	
-		echo 'Catégorie "'.$type.' : '.$name.'" ajoutée avec succes !<br>';	
-		$id = category_exists($name);
-	}
-	return $id;
-}
-echo '<br><br>';
-// script articles
+$cat_mere = "Marques auto";
 $args = array(
     'hide_empty'          => 0,
     'order'               => 'ASC',
@@ -63,7 +17,9 @@ foreach($categories as $category) {
    	$parent_name = get_cat_name($parent);
 
    	// si la catégorie est un modèle
-   	if($parent_name != "" && $parent_name != $cat_name){  
+   	if($parent_name != "" && $parent_name != $cat_mere){   	
+	   	echo '- '.$category->term_id.' : '.$category->name.'<br>';
+	   	echo '==> '.$parent.' : '.$parent_name.'<br>';
 
 	   	$title = wp_strip_all_tags($parent_name." - ".$category->name);
 	   	$content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
